5590d4bd361cebceb158ea66d765df642a818b00
benyltam <ben@welkin.com.hk>
2022-08-24 21:45:13 +0800

    Add files via upload

---
+++ BaseControlLibrary/ClassMiddle.cs
@@ -0,0 +1,10 @@
+﻿using Library;
+using NewTims;
+
+namespace BaseControlLibrary
+{
+    public class ClassMiddle
+    {
+        static Lib Lib = new Lib();
+    }
+}
---
+++ NewTims/Student_Form.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace NewTims
+{
+    partial class Student_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StudnetList_dataListView = new BrightIdeasSoftware.DataListView();
+            this.Surname_textBox = new System.Windows.Forms.TextBox();
+            this.Surname_label = new System.Windows.Forms.Label();
+            this.GivenName_label = new System.Windows.Forms.Label();
+            this.GivenName_textBox = new System.Windows.Forms.TextBox();
+            this.MiddleName_label = new System.Windows.Forms.Label();
+            this.MiddleName_textBox = new System.Windows.Forms.TextBox();
+            this.Institution_label = new System.Windows.Forms.Label();
+            this.Title_TextBox = new System.Windows.Forms.TextBox();
+            this.Institution_radioButton = new System.Windows.Forms.RadioButton();
+            this.ClientType_panel = new System.Windows.Forms.Panel();
+            this.Client_radioButton = new System.Windows.Forms.RadioButton();
+            this.StudentID_textBox = new System.Windows.Forms.TextBox();
+            this.StudentID_label = new System.Windows.Forms.Label();
+            this.Sex_label = new System.Windows.Forms.Label();
+            this.Remark_textBox = new System.Windows.Forms.TextBox();
+            this.ContactPerson_label = new System.Windows.Forms.Label();
+            this.ContactPerson_textBox = new System.Windows.Forms.TextBox();
+            this.Sex_comboBox = new System.Windows.Forms.ComboBox();
+            this.Course_dataListView = new BrightIdeasSoftware.DataListView();
+            this.Remark_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.StudnetList_dataListView)).BeginInit();
+            this.ClientType_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Course_dataListView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StudnetList_dataListView
+            // 
+            this.StudnetList_dataListView.CellEditUseWholeCell = false;
+            this.StudnetList_dataListView.DataSource = null;
+            this.StudnetList_dataListView.HideSelection = false;
+            this.StudnetList_dataListView.Location = new System.Drawing.Point(9, 11);
+            this.StudnetList_dataListView.Name = "StudnetList_dataListView";
+            this.StudnetList_dataListView.Size = new System.Drawing.Size(209, 269);
+            this.StudnetList_dataListView.TabIndex = 0;
+            this.StudnetList_dataListView.UseCompatibleStateImageBehavior = false;
+            this.StudnetList_dataListView.View = System.Windows.Forms.View.Details;
+            // 
+            // Surname_textBox
+            // 
+            this.Surname_textBox.Location = new System.Drawing.Point(276, 23);
+            this.Surname_textBox.Name = "Surname_textBox";
+            this.Surname_textBox.Size = new System.Drawing.Size(87, 20);
+            this.Surname_textBox.TabIndex = 4;
+            // 
+            // Surname_label
+            // 
+            this.Surname_label.AutoSize = true;
+            this.Surname_label.Location = new System.Drawing.Point(224, 26);
+            this.Surname_label.Name = "Surname_label";
+            this.Surname_label.Size = new System.Drawing.Size(49, 13);
+            this.Surname_label.TabIndex = 3;
+            this.Surname_label.Text = "Surname";
+            // 
+            // GivenName_label
+            // 
+            this.GivenName_label.AutoSize = true;
+            this.GivenName_label.Location = new System.Drawing.Point(370, 26);
+            this.GivenName_label.Name = "GivenName_label";
+            this.GivenName_label.Size = new System.Drawing.Size(66, 13);
+            this.GivenName_label.TabIndex = 5;
+            this.GivenName_label.Text = "Given Name";
+            // 
+            // GivenName_textBox
+            // 
+            this.GivenName_textBox.Location = new System.Drawing.Point(437, 23);
+            this.GivenName_textBox.Name = "GivenName_textBox";
+            this.GivenName_textBox.Size = new System.Drawing.Size(152, 20);
+            this.GivenName_textBox.TabIndex = 5;
+            // 
+            // MiddleName_label
+            // 
+            this.MiddleName_label.AutoSize = true;
+            this.MiddleName_label.Location = new System.Drawing.Point(596, 26);
+            this.MiddleName_label.Name = "MiddleName_label";
+            this.MiddleName_label.Size = new System.Drawing.Size(69, 13);
+            this.MiddleName_label.TabIndex = 6;
+            this.MiddleName_label.Text = "Middle Name";
+            // 
+            // MiddleName_textBox
+            // 
+            this.MiddleName_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MiddleName_textBox.Location = new System.Drawing.Point(666, 23);
+            this.MiddleName_textBox.Name = "MiddleName_textBox";
+            this.MiddleName_textBox.Size = new System.Drawing.Size(114, 20);
+            this.MiddleName_textBox.TabIndex = 7;
+            this.MiddleName_textBox.WordWrap = false;
+            // 
+            // Institution_label
+            // 
+            this.Institution_label.AutoSize = true;
+            this.Institution_label.Location = new System.Drawing.Point(223, 26);
+            this.Institution_label.Name = "Institution_label";
+            this.Institution_label.Size = new System.Drawing.Size(52, 13);
+            this.Institution_label.TabIndex = 3;
+            this.Institution_label.Text = "Institution";
+            // 
+            // Title_TextBox
+            // 
+            this.Title_TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Title_TextBox.Location = new System.Drawing.Point(276, 23);
+            this.Title_TextBox.Name = "Title_TextBox";
+            this.Title_TextBox.Size = new System.Drawing.Size(504, 20);
+            this.Title_TextBox.TabIndex = 3;
+            // 
+            // Institution_radioButton
+            // 
+            this.Institution_radioButton.AutoSize = true;
+            this.Institution_radioButton.Location = new System.Drawing.Point(60, 0);
+            this.Institution_radioButton.Name = "Institution_radioButton";
+            this.Institution_radioButton.Size = new System.Drawing.Size(70, 17);
+            this.Institution_radioButton.TabIndex = 2;
+            this.Institution_radioButton.Text = "Institution";
+            this.Institution_radioButton.UseVisualStyleBackColor = true;
+            this.Institution_radioButton.CheckedChanged += new System.EventHandler(this.Institution_radioButton_CheckedChanged);
+            // 
+            // ClientType_panel
+            // 
+            this.ClientType_panel.Controls.Add(this.Client_radioButton);
+            this.ClientType_panel.Controls.Add(this.Institution_radioButton);
+            this.ClientType_panel.Location = new System.Drawing.Point(224, 4);
+            this.ClientType_panel.Name = "ClientType_panel";
+            this.ClientType_panel.Size = new System.Drawing.Size(131, 16);
+            this.ClientType_panel.TabIndex = 2;
+            // 
+            // Client_radioButton
+            // 
+            this.Client_radioButton.AutoSize = true;
+            this.Client_radioButton.Checked = true;
+            this.Client_radioButton.Location = new System.Drawing.Point(3, 0);
+            this.Client_radioButton.Name = "Client_radioButton";
+            this.Client_radioButton.Size = new System.Drawing.Size(51, 17);
+            this.Client_radioButton.TabIndex = 1;
+            this.Client_radioButton.TabStop = true;
+            this.Client_radioButton.Text = "Client";
+            this.Client_radioButton.UseVisualStyleBackColor = true;
+            this.Client_radioButton.CheckedChanged += new System.EventHandler(this.Client_radioButton_CheckedChanged);
+            // 
+            // StudentID_textBox
+            // 
+            this.StudentID_textBox.Location = new System.Drawing.Point(286, 49);
+            this.StudentID_textBox.Name = "StudentID_textBox";
+            this.StudentID_textBox.Size = new System.Drawing.Size(45, 20);
+            this.StudentID_textBox.TabIndex = 11;
+            // 
+            // StudentID_label
+            // 
+            this.StudentID_label.AutoSize = true;
+            this.StudentID_label.Location = new System.Drawing.Point(224, 53);
+            this.StudentID_label.Name = "StudentID_label";
+            this.StudentID_label.Size = new System.Drawing.Size(58, 13);
+            this.StudentID_label.TabIndex = 10;
+            this.StudentID_label.Text = "Student ID";
+            // 
+            // Sex_label
+            // 
+            this.Sex_label.AutoSize = true;
+            this.Sex_label.Location = new System.Drawing.Point(343, 53);
+            this.Sex_label.Name = "Sex_label";
+            this.Sex_label.Size = new System.Drawing.Size(42, 13);
+            this.Sex_label.TabIndex = 11;
+            this.Sex_label.Text = "Gender";
+            // 
+            // Remark_textBox
+            // 
+            this.Remark_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Remark_textBox.Location = new System.Drawing.Point(227, 84);
+            this.Remark_textBox.Multiline = true;
+            this.Remark_textBox.Name = "Remark_textBox";
+            this.Remark_textBox.Size = new System.Drawing.Size(555, 77);
+            this.Remark_textBox.TabIndex = 16;
+            // 
+            // ContactPerson_label
+            // 
+            this.ContactPerson_label.AutoSize = true;
+            this.ContactPerson_label.Location = new System.Drawing.Point(519, 55);
+            this.ContactPerson_label.Name = "ContactPerson_label";
+            this.ContactPerson_label.Size = new System.Drawing.Size(80, 13);
+            this.ContactPerson_label.TabIndex = 13;
+            this.ContactPerson_label.Text = "Contact Person";
+            // 
+            // ContactPerson_textBox
+            // 
+            this.ContactPerson_textBox.Location = new System.Drawing.Point(605, 49);
+            this.ContactPerson_textBox.Name = "ContactPerson_textBox";
+            this.ContactPerson_textBox.Size = new System.Drawing.Size(124, 20);
+            this.ContactPerson_textBox.TabIndex = 19;
+            // 
+            // Sex_comboBox
+            // 
+            this.Sex_comboBox.FormattingEnabled = true;
+            this.Sex_comboBox.Items.AddRange(new object[] {
+            "Organization",
+            "Male",
+            "Female"});
+            this.Sex_comboBox.Location = new System.Drawing.Point(392, 49);
+            this.Sex_comboBox.Name = "Sex_comboBox";
+            this.Sex_comboBox.Size = new System.Drawing.Size(76, 21);
+            this.Sex_comboBox.TabIndex = 12;
+            this.Sex_comboBox.SelectedIndexChanged += new System.EventHandler(this.Sex_comboBox_SelectedIndexChanged);
+            // 
+            // Course_dataListView
+            // 
+            this.Course_dataListView.CellEditUseWholeCell = false;
+            this.Course_dataListView.DataSource = null;
+            this.Course_dataListView.HideSelection = false;
+            this.Course_dataListView.Location = new System.Drawing.Point(227, 178);
+            this.Course_dataListView.Name = "Course_dataListView";
+            this.Course_dataListView.Size = new System.Drawing.Size(206, 104);
+            this.Course_dataListView.TabIndex = 21;
+            this.Course_dataListView.UseCompatibleStateImageBehavior = false;
+            this.Course_dataListView.View = System.Windows.Forms.View.Details;
+            // 
+            // Remark_label
+            // 
+            this.Remark_label.AutoSize = true;
+            this.Remark_label.Location = new System.Drawing.Point(224, 70);
+            this.Remark_label.Name = "Remark_label";
+            this.Remark_label.Size = new System.Drawing.Size(44, 13);
+            this.Remark_label.TabIndex = 23;
+            this.Remark_label.Text = "Remark";
+            // 
+            // Student_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 289);
+            this.Controls.Add(this.Remark_label);
+            this.Controls.Add(this.Course_dataListView);
+            this.Controls.Add(this.Sex_comboBox);
+            this.Controls.Add(this.ContactPerson_textBox);
+            this.Controls.Add(this.ContactPerson_label);
+            this.Controls.Add(this.Remark_textBox);
+            this.Controls.Add(this.Sex_label);
+            this.Controls.Add(this.StudentID_label);
+            this.Controls.Add(this.StudentID_textBox);
+            this.Controls.Add(this.ClientType_panel);
+            this.Controls.Add(this.MiddleName_label);
+            this.Controls.Add(this.MiddleName_textBox);
+            this.Controls.Add(this.GivenName_label);
+            this.Controls.Add(this.GivenName_textBox);
+            this.Controls.Add(this.Surname_label);
+            this.Controls.Add(this.Surname_textBox);
+            this.Controls.Add(this.StudnetList_dataListView);
+            this.Controls.Add(this.Institution_label);
+            this.Controls.Add(this.Title_TextBox);
+            this.Name = "Student_Form";
+            this.Text = "Student";
+            this.Load += new System.EventHandler(this.Student_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.StudnetList_dataListView)).EndInit();
+            this.ClientType_panel.ResumeLayout(false);
+            this.ClientType_panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Course_dataListView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private BrightIdeasSoftware.DataListView StudnetList_dataListView;
+        private System.Windows.Forms.TextBox Surname_textBox;
+        private System.Windows.Forms.Label Surname_label;
+        private System.Windows.Forms.Label GivenName_label;
+        private System.Windows.Forms.TextBox GivenName_textBox;
+        private System.Windows.Forms.Label MiddleName_label;
+        private System.Windows.Forms.TextBox MiddleName_textBox;
+        private System.Windows.Forms.Label Institution_label;
+        private System.Windows.Forms.TextBox Title_TextBox;
+        private System.Windows.Forms.RadioButton Institution_radioButton;
+        private System.Windows.Forms.Panel ClientType_panel;
+        private System.Windows.Forms.RadioButton Client_radioButton;
+        private System.Windows.Forms.TextBox StudentID_textBox;
+        private System.Windows.Forms.Label StudentID_label;
+        private System.Windows.Forms.Label Sex_label;
+        private System.Windows.Forms.TextBox Remark_textBox;
+        private System.Windows.Forms.Label ContactPerson_label;
+        private System.Windows.Forms.TextBox ContactPerson_textBox;
+        private System.Windows.Forms.ComboBox Sex_comboBox;
+        private BrightIdeasSoftware.DataListView Course_dataListView;
+        private System.Windows.Forms.Label Remark_label;
+    }
+}
+
