f06dcf55c2c4bb78a2a94969210cd2bff7c57f6b
benyltam <ben@welkin.com.hk>
2022-10-02 12:09:33 +0800

    Add files via upload

---
+++ NewTims/NewTims/BaseForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace NewTims
+{
+    public partial class BaseForm : Form
+    {
+        public BaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BaseForm_Load(object sender, EventArgs e)
+        {
+            //Lib Lib = new Lib();
+        }
+
+        private void ResizeForm(object sender, EventArgs e)
+        {
+            //foreach (var p in this.Controls.OfType<BaseControlLibrary.Panel>())
+            foreach (Control c in this.Controls)
+            {
+                try
+                {
+                    ResizeForm(c, e);
+                }
+                catch { }
+            }
+        }
+    }
+}
+
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
