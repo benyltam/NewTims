03c1f478b9a93cf0fd9159f75f5d37eebfda7d8d
benyltam <ben@welkin.com.hk>
2022-10-02 12:19:10 +0800

    Add files via upload

---
+++ BaseControlLibrary/BaseControlLibrary/Button.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using Library;
+
+
+namespace BaseControlLibrary
+{
+    [ToolboxBitmap(@"Button.bmp")]
+    public partial class Button : UserControl
+    {
+        static int DefaultTop;
+        static int DefaultLeft;
+        static int DefaultHeight;
+        static int DefaultWidth;
+
+        static int ParentDefaultHeight;
+        static int ParentDefaultWidth;
+        static int ParentHeight;
+        static int ParentWidth;
+        static string ResizeSetting;
+
+        private readonly Lib lib = new Lib();
+
+        public Button()
+        {
+            DefaultTop = Top;
+            DefaultLeft = Left;
+            DefaultHeight = Height;
+            DefaultWidth = Width;
+            ResizeSetting = "";
+
+            InitializeComponent();
+            lib.RestoreCtrlSize();
+
+            int height = Height;
+            int width = Width;
+            int top = Top;
+            int left = Left;
+
+            //public void ResizeCtrl(string str, ref int ParentHeight, int ParentDefaultHeight, ref int ParentWidth, int ParentDefaultWidth,
+            //    ref int height, ref int width, ref int top, ref int left,
+            //    int defaultHeight, int defaultWidth, int defaultTop, int defaultLeft)
+            if (!string.IsNullOrEmpty(ResizeSetting))
+            lib.ResizeCtrl(ResizeSetting, ref ParentHeight, ParentDefaultHeight, ref ParentWidth, ParentDefaultWidth
+                , ref height, ref width, ref top, ref left
+                , DefaultHeight, DefaultWidth, DefaultTop, DefaultLeft);
+        }
+    }
+}
---
+++ BaseControlLibrary/BaseControlLibrary/Panel.cs
@@ -0,0 +1,41 @@
+﻿using Library;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BaseControlLibrary
+{
+    //[ToolboxBitmap(@"Panel.bmp")]
+    public partial class Panel : UserControl
+    {
+        static int DefaultTop;
+        static int DefaultLeft;
+        static int DefaultHeight;
+        static int DefaultWidth;
+        private Lib lib;
+        public Panel()
+        {
+            int DefaultTop = this.Top;
+            int DefaultLeft = this.Left;
+            int DefaultHeight = this.Height;
+            int DefaultWidth = this.Width;
+            string ResizeSetting = "";
+
+            InitializeComponent();
+
+            Lib lib = new Lib();
+            int ParentHeight = Parent.Height;
+            int ParentWidth = Parent.Width;
+            int ParentDefaultWidth = 0;
+            int ParentDefaultHeight = 0;
+            int height = Height;
+            int width = Width;
+            int top = Top;
+            int left = Left;
+
+            if (!string.IsNullOrEmpty(ResizeSetting))
+                lib.ResizeCtrl(ResizeSetting, ref ParentHeight, ParentDefaultHeight, ref ParentWidth, ParentDefaultWidth
+                    , ref height, ref width, ref top, ref left, DefaultHeight, DefaultWidth, DefaultTop, DefaultLeft);
+
+        }
+    }
+}
