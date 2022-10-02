8cfb82e034ccf9c2bb3bf75df57fcd1a4593b4a3
benyltam <ben@welkin.com.hk>
2022-10-02 12:16:15 +0800

    Add files via upload

---
+++ NewTims/BaseControlLibrary/Button.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaseControlLibrary
+{
+    [ToolboxBitmap(@"Button.bmp")]
+    public partial class Button : UserControl
+    {
+        static int DefaultTop;
+        static int DefaultLeft;
+        static int DefaultHeight;
+        static int DefaultWidth;
+        static Lib Lib = new Lib();
+
+        public Button()
+        {
+            InitializeComponent();
+            DefaultTop = Top;
+            DefaultLeft = Left;
+            DefaultHeight = Height;
+            DefaultWidth = Width;
+        }
+        Lib.ResizeCtrl();
+    }
+}
---
+++ NewTims/BaseControlLibrary/CheckBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaseControlLibrary
+{
+    public partial class CheckBox : UserControl
+    {
+        //Lib lib = new Lib();
+        static int DefaultTop;
+        static int DefaultLeft;
+        static int DefaultHeight;
+        static int DefaultWidth;
+
+        public CheckBox()
+        {
+            InitializeComponent();
+            int DefaultTop = this.Top;
+            int DefaultLeft = this.Left;
+            int DefaultHeight = this.Height;
+            int DefaultWidth = this.Width;
+        }
+
+        public void Resize()
+        {
+            //Lib.ResizeCtl();
+        }
+
+        private void XcheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
